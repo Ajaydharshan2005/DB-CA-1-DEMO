557e14f1557d6f8e6f56a59bd64396c69a66e530
Ajay Dharshan <ajaydharshan2005@gmail.com>
2025-05-15 14:32:35 +0530

    done

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const port=5000;
+const app=express();
+app.use(express.json())
+
+app.get('/',(req,res)=>{
+    res.send('Restaurant api running')
+})
+
+
+const resSchema= new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    location:{
+        type:String,
+        required:true
+    },
+    menuitem:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Menu"
+    }]
+
+})
+const res=mongoose.model('Res',resSchema)
+
+const menuSchema=new mongoose.Schema({
+    dishname:{
+        type:String,
+        required:true
+    },
+    cost:{
+        type:Number,
+        required:true
+    }
+})
+
+const menu=mongoose.model('Menu',menuSchema)
+
+
+app.listen(port,()=>{
+    console.log(`server running on http://localhost:${port}`)
+})
+
